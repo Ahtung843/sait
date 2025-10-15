170b4e5a81631635c2cb0f57f002dd969713db78
Ahtyng843 <nizamoffrish@gmail.com>
2025-10-15 20:53:27 +0300

    Создал сайт

--- main/app.py
+++ main/app.py
@@ -2,11 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-FLASK_ENV = "production"
-SECRET_KEY = "b207674a1396dc9ed49df9e3e9614a28"
-PYTHON_VERSION = "3.13"
-PORT = None
-
 
 @app.route('/')
 def home():
